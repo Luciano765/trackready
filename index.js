5dee145212752e729f3a29534b445bec97875a9f
Luciano Tjitrodipo <lucianotjitro@gmail.com>
2026-07-08 11:01:06 +0200

    Add first Javascript interaction

---
+++ index.js
@@ -0,0 +1,6 @@
+let subscribedMessage = "Only 1 spot left!"
+let featuredStatus = document.getElementById("featuredStatus")
+
+function signUp() {
+    featuredStatus.innerText = subscribedMessage
+}
